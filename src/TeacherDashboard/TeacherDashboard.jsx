96124ce56c3c6836991e12b165eae18277f967ec
avinashnadkar <avinashnadkar3@gmail.com>
2021-08-08 10:08:39 +0530

    Created teacher dashboard and Loin

---
+++ src/TeacherDashboard/TeacherDashboard.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import axios from "axios";
+import Video from "../component/VideoWindow/Video";
+
+const TeacherDashboard = () => {
+
+
+    return(
+         <div>
+             <h3>Teacher Dashboard</h3>
+             <Video/>
+         </div>
+    )
+}
+
+
+export default TeacherDashboard;
+
